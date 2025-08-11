b8f744e494d29f8f5d3c7c68da180d7af00e4fdd
MARCELO SEPULVEDA <sepulveda.mf2025@gmail.com>
2025-08-11 19:39:34 +0000

    SACALA,

--- src/components/layout/app-header.tsx
+++ src/components/layout/app-header.tsx
@@ -37,29 +37,6 @@ export default function AppHeader() {
           <span className="sr-only">Toggle theme</span>
         </Button>
         */}
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="relative h-9 w-9 rounded-full">
-              <Avatar className="h-9 w-9">
-                <AvatarImage src="https://placehold.co/100x100.png" alt="@user" data-ai-hint="person face" />
-                <AvatarFallback>JD</AvatarFallback>
-              </Avatar>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56" align="end">
-            <DropdownMenuLabel>
-              <p>Jane Doe</p>
-              <p className="text-xs font-normal text-muted-foreground">
-                jane.doe@aptech.com
-              </p>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Settings</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Log out</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
       </div>
     </header>
   );
