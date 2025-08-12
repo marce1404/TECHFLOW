61463bd2fc6d178c365d9fc23bfd44350b001971
MARCELO SEPULVEDA <sepulveda.mf2025@gmail.com>
2025-08-12 17:38:10 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  projectId: 'techflow-operations-manager',
+  appId: '1:680065190644:web:8ac04a3990094f82696ca1',
+  storageBucket: 'techflow-operations-manager.firebasestorage.app',
+  apiKey: 'AIzaSyBNg61wt8k2Bd9MRGjQTEw6Vj6q6UbPrJM',
+  authDomain: 'techflow-operations-manager.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '680065190644',
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+
+export { app, db };
