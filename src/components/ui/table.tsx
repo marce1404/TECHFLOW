d620d7fbd1e27876636c925027ddd1feab79ec09
MARCELO SEPULVEDA <sepulveda.mf2025@gmail.com>
2025-08-13 00:28:33 +0000

    HISTORIAL. TIENE SCROLL HORISOTAL, QUITALO Y QUE SE AUTOAJUSTE A LA PANT

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -6,13 +6,11 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="relative w-full overflow-auto">
     <table
       ref={ref}
-      className={cn("w-full caption-bottom text-sm", className)}
+      className={cn("w-full caption-bottom text-sm table-fixed", className)}
       {...props}
     />
-  </div>
 ))
 Table.displayName = "Table"
 
@@ -87,7 +85,7 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <td
     ref={ref}
-    className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
+    className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0 break-words", className)}
     {...props}
   />
 ))
