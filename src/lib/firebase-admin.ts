20331284375a6d8f12952cac081ee01301289301
MARCELO SEPULVEDA <sepulveda.mf2025@gmail.com>
2025-08-21 18:28:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,21 +1,19 @@
 
+'use server';
 import * as admin from 'firebase-admin';
 
-const getAdminApp = () => {
-    if (admin.apps.length > 0) {
-        return admin.apps[0]!;
-    }
-
+if (!admin.apps.length) {
     const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY
         ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
         : undefined;
-
-    return admin.initializeApp({
+        
+    admin.initializeApp({
         credential: serviceAccount
             ? admin.credential.cert(serviceAccount)
             : admin.credential.applicationDefault(),
     });
-};
+}
 
-export const adminApp = getAdminApp();
+
+export const adminApp = admin.apps[0]!;
 export const adminDb = admin.firestore();
