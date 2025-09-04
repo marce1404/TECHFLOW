d6011211ec55ba115e5c38fe981a299715cd7b7d
MARCELO SEPULVEDA <sepulveda.mf2025@gmail.com>
2025-09-04 12:25:34 +0000

    2025-09-04T12:23:45.079Z [error] ⨯ Error [GenkitError]: FAILED_PRECONDIT

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,5 +1,9 @@
+
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const ai = genkit({
   plugins: [googleAI()],
