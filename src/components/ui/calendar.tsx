761915f851497ce2e4c2d623244366cdc60f71ef
MARCELO SEPULVEDA <sepulveda.mf2025@gmail.com>
2025-09-22 17:34:51 +0000

    ok, esta bien el calendario, pero recuerda que el calendario debe poder

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -19,13 +19,16 @@ function Calendar({
 }: CalendarProps) {
   return (
     <DayPicker
+      captionLayout="dropdown-buttons"
+      fromYear={2015}
+      toYear={new Date().getFullYear() + 5}
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        caption_label: "text-sm font-medium hidden",
         caption_dropdowns: "flex justify-center gap-1",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
